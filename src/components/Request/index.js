c0a12a65ae3d9a6ae27d2f13e884e3df643d9b93
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2020-11-12 14:12:49 -0800

    requests now rendering on page

--- src/components/Request/index.js
+++ src/components/Request/index.js
@@ -1,6 +1,23 @@
 import React from 'react'
-export default function RequestCard({title}) {
+export default function RequestCard(props) {
     return (
-        <div>{title}</div>
+        <div className="requestCard">
+            <h2>
+                {props.username}
+            </h2>
+            <p>
+                {props.email}
+            </p>
+            <p>
+                {props.cafe_name}
+            </p>
+            <p>
+                {props.cafe_address}
+            </p>
+            <p>
+                {props.notes}
+            </p>
+            <button onClick={()=>props.deleteRequest(props.id)}>Delete</button>
+        </div>
     )
 }
